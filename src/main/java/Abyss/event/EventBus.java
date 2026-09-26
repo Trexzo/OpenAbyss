822e4c2be0fbbb8c403aaaf6e5928b2e3f30bbc2
Trexzo <45512387+Trexzo@users.noreply.github.com>
2026-09-26 03:53:59 -0300

    Fix EventBus self-test Arrays import

--- src/main/java/Abyss/event/EventBus.java
+++ src/main/java/Abyss/event/EventBus.java
@@ -14,6 +14,7 @@ import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
